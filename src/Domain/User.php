4379496f920700c57d96269ead2aed68d1d79164
Guikingone <guillaume.loulier@hotmail.fr>
2016-06-02 09:52:11 +0200

    [!C] || Ajout Back-office|En cours

--- src/Domain/User.php
+++ src/Domain/User.php
@@ -1,151 +1,111 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Guillaume
- * Date: 08/04/2016
- * Time: 09:08
- */
 
-namespace WebLinks\Domain;
+namespace Weblinks\Domain;
 
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class User implements UserInterface
 {
-
     /**
-     * User id
+     * User id.
      *
      * @var integer
      */
     private $id;
 
     /**
-     * User name
+     * User name.
      *
      * @var string
      */
-    private $name;
+    private $username;
 
     /**
-     * User password
+     * User password.
      *
      * @var string
      */
     private $password;
 
     /**
-     * Originally used to encrypt the password
+     * Salt that was originally used to encode the password.
      *
      * @var string
      */
     private $salt;
 
     /**
-     * Roles
-     * Value : ROLE_USER or ROLE_ADMIN
+     * Role.
+     * Values : ROLE_USER or ROLE_ADMIN.
      *
      * @var string
      */
     private $role;
 
-    /**
-     * @return int
-     */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
-    /**
-     * @param $id
-     */
-    public function setId($id)
-    {
+    public function setId($id) {
         $this->id = $id;
     }
 
     /**
      * @inheritDoc
-     * @return string
      */
-    public function getUsername()
-    {
-        return $this->name;
+    public function getUsername() {
+        return $this->username;
     }
 
-    /**
-     * @param $name
-     */
-    public function setUsername($name)
-    {
-        $this->name = $name;
+    public function setUsername($username) {
+        $this->username = $username;
     }
 
     /**
      * @inheritDoc
-     * @return string
      */
-    public function getPassword()
-    {
+    public function getPassword() {
         return $this->password;
     }
 
-    /**
-     * @param $password
-     */
-    public function setPassword($password)
-    {
+    public function setPassword($password) {
         $this->password = $password;
     }
 
     /**
      * @inheritDoc
-     * @return string
      */
     public function getSalt()
     {
         return $this->salt;
     }
 
-    /**
-     * @param $salt
-     */
     public function setSalt($salt)
     {
         $this->salt = $salt;
     }
 
-    /**
-     * @return string
-     */
     public function getRole()
     {
         return $this->role;
     }
 
-    /**
-     * @param $role
-     */
-    public function setRole($role)
-    {
+    public function setRole($role) {
         $this->role = $role;
     }
 
     /**
      * @inheritDoc
-     * @return array
      */
     public function getRoles()
     {
-        return array($this->getRoles());
+        return array($this->getRole());
     }
 
     /**
      * @inheritDoc
      */
-    public function eraseCredentials()
-    {
+    public function eraseCredentials() {
         // Nothing to do here
     }
 }
